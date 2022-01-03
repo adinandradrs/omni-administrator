fa05866f1144184642803a3e7ff7cdc4b29e2674
nandra <adinandra.dharmasurya@gmail.com>
2022-01-04 01:11:14 +0700

    Update FormUserSearchBean.java

--- src/main/java/id/codefun/omni/administrator/beans/user/FormUserSearchBean.java
+++ src/main/java/id/codefun/omni/administrator/beans/user/FormUserSearchBean.java
@@ -2,7 +2,6 @@ package id.codefun.omni.administrator.beans.user;
 
 import javax.annotation.ManagedBean;
 import javax.faces.view.ViewScoped;
-
 import id.codefun.omni.administrator.beans.BaseFormBean;
 import lombok.extern.slf4j.Slf4j;
 
