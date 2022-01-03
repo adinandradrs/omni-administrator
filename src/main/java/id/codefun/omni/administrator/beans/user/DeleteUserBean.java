efe09d5499081b87232edff97022d5e7986f52b3
nandra <adinandra.dharmasurya@gmail.com>
2022-01-04 01:11:12 +0700

    Update DeleteUserBean.java

--- src/main/java/id/codefun/omni/administrator/beans/user/DeleteUserBean.java
+++ src/main/java/id/codefun/omni/administrator/beans/user/DeleteUserBean.java
@@ -1,9 +1,7 @@
 package id.codefun.omni.administrator.beans.user;
 
 import javax.annotation.ManagedBean;
-
 import org.springframework.web.context.annotation.RequestScope;
-
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
